29aed3aeb6890a38d3df7c75643bc2e97dc2f1f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-20 19:41:39 +0000

    Deploy Yog9/Metflix to github.com/Yog9/Metflix.git:gh-pages

---
+++ precache-manifest.9a6f678d7f53c11d02d76a77169934ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b701edcf0960d9716a39e1c3da1a33",
+    "url": "/Metflix/index.html"
+  },
+  {
+    "revision": "fa9d3e148b70a893f23a",
+    "url": "/Metflix/static/css/main.d09bb36c.chunk.css"
+  },
+  {
+    "revision": "a7a60b2b8f3503f592a2",
+    "url": "/Metflix/static/js/2.14277c96.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Metflix/static/js/2.14277c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa9d3e148b70a893f23a",
+    "url": "/Metflix/static/js/main.a7650072.chunk.js"
+  },
+  {
+    "revision": "0e48beb4f67b0c6be9e8",
+    "url": "/Metflix/static/js/runtime-main.15813dc2.js"
+  },
+  {
+    "revision": "73f0bcaf5385e0980e347b81fcb2e93a",
+    "url": "/Metflix/static/media/no-image.73f0bcaf.png"
+  }
+]);
